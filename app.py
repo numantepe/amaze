86fe752a8d94d2ae8c727ba1143551f2256179bf
Numan Tepe <ynumantepe@gmail.com>
2022-08-10 22:54:32 -0400

    implement sending data to server

--- app.py
+++ app.py
@@ -5,3 +5,17 @@ app = Flask(__name__)
 @app.route("/")
 def welcome_page():
     return render_template("index.html")
+
+@app.route("/search-product", methods=["POST"])
+def search_product():
+    print("search_product:", request.form.get("search_product"))
+    print("min_price:", request.form.get("min_price"))
+    print("max_price:", request.form.get("max_price"))
+    print("min_num_of_stars:", request.form.get("min_num_of_stars"))
+    print("max_num_of_stars:", request.form.get("max_num_of_stars"))
+    print("min_num_of_ratings:", request.form.get("min_num_of_ratings"))
+    print("max_num_of_ratings:", request.form.get("max_num_of_ratings"))
+    print("max_num_of_products:", request.form.get("max_num_of_products"))
+    print("chosen_op:", request.form.get("chosen_op"))
+
+    return "OOOOOOOKKKKKK"
--- static/ts/script.ts
+++ static/ts/script.ts
@@ -1,3 +1,53 @@
 {
+let webURL : string = "http://127.0.0.1:5000";
+let search_product = document.querySelector("#search-product") as HTMLInputElement;
+let min_price = document.querySelector("#min-price") as HTMLInputElement;
+let max_price = document.querySelector("#min-price") as HTMLInputElement;
+let min_num_of_stars = document.querySelector("#min-num-of-stars") as HTMLInputElement;
+let max_num_of_stars = document.querySelector("#max-num-of-stars") as HTMLInputElement;
+let min_num_of_ratings = document.querySelector("#min-num-of-ratings") as HTMLInputElement;
+let max_num_of_ratings = document.querySelector("#max-num-of-ratings") as HTMLInputElement;
+let max_num_of_products = document.querySelector("#max-num-of-products") as HTMLInputElement;
+let chosen_op : string = "op1";
 
+let send_message_to_server_list = document.querySelectorAll(".send-message-to-server");   
+for(let i = 0; i < send_message_to_server_list.length; i++)
+{
+    let send_message_to_server = send_message_to_server_list[i] as HTMLElement; 
+
+    send_message_to_server.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        if(e.target instanceof Element){
+            if(e.target.classList.contains("option-btn")){
+                let options = document.querySelectorAll(".option-btn");
+                for(let i = 0; i < options.length; i++)
+                {
+                    (options[i]).classList.remove("chosen-option");
+                }
+                e.target.classList.add("chosen-option");
+                chosen_op = e.target.id;
+            }
+        }
+
+        let xhttp = new XMLHttpRequest();
+
+        xhttp.onreadystatechange = function() {
+            if (this.readyState === 4 && this.status === 200){
+                console.log("Response Type " + this.responseType);
+                console.log("Response Text " + this.responseText);
+                }
+            }
+        
+        const apiURL = `${webURL}/search-product`;
+        xhttp.open("POST", apiURL, true);
+        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhttp.send("search_product="+search_product.value.trim()+"&min_price="+min_price.value.trim()
+                    +"&max_price="+max_price.value.trim()
+                    +"&min_num_of_stars="+min_num_of_stars.value.trim()+"&max_num_of_stars="+max_num_of_stars.value.trim()
+                    +"&min_num_of_ratings="+min_num_of_ratings.value.trim()
+                    +"&max_num_of_ratings="+max_num_of_ratings.value.trim()
+                    +"&max_num_of_products="+max_num_of_products.value.trim()+"&chosen_op="+chosen_op);
+    });
+}
 }
