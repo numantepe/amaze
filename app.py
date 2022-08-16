ed57a941514bfb31f78d64072c4f16316d159e16
Numan Tepe <ynumantepe@gmail.com>
2022-08-15 21:58:21 -0400

    implement filter functionality and send json data to client

--- app.py
+++ app.py
@@ -2,15 +2,6 @@ from flask import Flask, request, render_template, jsonify, g
 from bs4 import BeautifulSoup
 import requests
 
-class Product:
-    def __init__(self, image = None, desc = None, url = None, price = None, stars = None, ratings = None):
-        self.image = image
-        self.desc = desc
-        self.url = url
-        self.price = price
-        self.stars = stars
-        self.ratings = ratings
-
 app = Flask(__name__)
 
 @app.route("/")
@@ -20,17 +11,7 @@ def welcome_page():
 @app.route("/search-product", methods=["POST"])
 def search_product():
     search_product = request.form.get("search_product")
-    min_price = request.form.get("min_price")
-    max_price = request.form.get("max_price")
-    min_num_of_stars = request.form.get("min_num_of_stars")
-    max_num_of_stars = request.form.get("max_num_of_stars")
-    min_num_of_ratings = request.form.get("min_num_of_ratings")
-    max_num_of_ratings = request.form.get("max_num_of_ratings")
-    max_num_of_products = request.form.get("max_num_of_products")
-    chosen_op = request.form.get("chosen_op") # op1 op2 op3 op4
     chosen_country = request.form.get("chosen_country")
-
-    # OPTION 4 WILL BE AS MANY STARS AND RATINGS AS POSSIBLE AND ALSO AS CHEAP AS POSSIBLE
 
     amazon_url = ""
     if(chosen_country == "ca"):
@@ -43,57 +24,77 @@ def search_product():
         search_amazon_url += word + "+"
     search_amazon_url = search_amazon_url[:-1]
 
-    headers = {
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-        #'referer': 'https://www.amazon.com/s?k=web+scraping+books&ref=nb_sb_noss',
-        'upgrade-insecure-requests': '1',
-        'cache-control': 'no-cache',
-        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'
-        }
+    product_set = set() 
 
-    r = requests.get(search_amazon_url, headers=headers)
+    requests_session = requests.Session()
 
-    while(r.status_code > 500):
-        r = requests.get(search_amazon_url, headers=headers)
+    for i in range(1, 8):
+        search_amazon_url = search_amazon_url + "&page=" + str(i)
 
-    html_doc = r.text
-    soup = BeautifulSoup(html_doc, 'lxml')
+        headers = {
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+            'referer': search_amazon_url + "&ref=nb_sb_noss",
+            'upgrade-insecure-requests': '1',
+            'cache-control': 'no-cache',
+            'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'
+            }
 
-    products = soup.select("div.s-result-item.s-asin")[3:]
+        r = requests_session.get(search_amazon_url, headers=headers)
 
-    with open("html.txt", "w") as f:
-        f.write(products[0].prettify())
-    
-    product_list = []
+        while(r.status_code > 500):
+            r = requests_session.get(search_amazon_url, headers=headers)
 
-    for product in products:
-        try:
-            p = Product()
+        html_doc = r.text
+        soup = BeautifulSoup(html_doc, 'lxml')
 
-            h2_desc_url = product.select("h2")[0]
-            p.desc = h2_desc_url.select("span.a-size-base-plus")[0].get_text()
-            p.url = amazon_url + h2_desc_url.select("a")[0].get("href")
+        products = soup.select("div.s-result-item.s-asin")[3:]
 
-            p.image = product.select("img.s-image")[0].get("src")
+        for product in products:
+            try:
+                h2_desc_url = product.select("h2")[0]
 
-            p.price = float(product.select("span.a-offscreen")[0].get_text()[1:])
+                desc = h2_desc_url.select("span.a-size-base-plus")[0].get_text()
+                url = amazon_url + h2_desc_url.select("a")[0].get("href")
+                image = product.select("img.s-image")[0].get("src")
 
-            div_stars_ratings = product.select("div.a-section.a-spacing-none.a-spacing-top-micro")[0]
-            p.stars = float(div_stars_ratings.select("span.a-icon-alt")[0].get_text()[:3])
-            p.ratings = int(div_stars_ratings.select("span.a-size-base.s-underline-text")[0].get_text().replace(',', ''))
-        except:
-            pass
+                price = float(product.select("span.a-offscreen")[0].get_text()[1:])
 
-        product_list.append(p)
+                div_stars_ratings = product.select("div.a-section.a-spacing-none.a-spacing-top-micro")[0]
 
-    for p in product_list:
-        print(p.desc)
-        print(p.url)
-        print(p.image)
-        print(p.price)
-        print(p.stars)
-        print(p.ratings)
-        print("=====================")
+                stars = float(div_stars_ratings.select("span.a-icon-alt")[0].get_text()[:3])
+                ratings = int(div_stars_ratings.select("span.a-size-base.s-underline-text")[0].get_text().replace(',', ''))
 
-    return  amazon_url
+                max_ratings_min_price = ratings / price 
+                max_stars_min_price = stars / price
+                max_ratings_max_stars = ratings / (5.0 - stars)
+                max_ratings_max_stars_min_price = (ratings / (5 - 0 - stars)) / price
+
+                t = (desc, url, image, price, stars, ratings, max_ratings_min_price,
+                    max_stars_min_price, max_ratings_max_stars, max_ratings_max_stars_min_price)
+
+                product_set.add(t)
+            except:
+                pass
+
+    print("length:", len(product_set))
+
+    #with open("items.txt", "w") as f:
+    #    for p in list(product_list):
+    #        for x in p:
+    #            f.write(str(x))
+    #            f.write("\n")
+    #        f.write("===============")
+    #    f.write("\n")
+
+    found = []
+
+    for p in product_set:
+        found.append({"desc" : p[0], "url" : p[1], "image" : p[2], 
+                        "price" : p[3], "stars" : p[4], "ratings" : p[5],
+                        "max_ratings_min_pric" : p[6],
+                        "max_stars_min_price" : p[7],
+                        "max_ratings_max_stars" : p[8],
+                        "max_ratings_max_stars_min_price" : p[9]})
+
+    return jsonify(found) 
--- static/ts/script.ts
+++ static/ts/script.ts
@@ -1,4 +1,17 @@
 {
+interface Product {
+    desc: string,
+    url: string,
+    image: string,
+    price: number,
+    stars: number,
+    ratings: number
+    max_ratings_min_price: number,
+    max_stars_min_price: number,
+    max_ratings_max_stars: number,
+    max_ratings_max_stars_min_price: number
+}
+
 let webURL : string = "http://127.0.0.1:5000";
 let search_product = document.querySelector("#search-product") as HTMLInputElement;
 let min_price = document.querySelector("#min-price") as HTMLInputElement;
@@ -11,6 +24,102 @@ let max_num_of_products = document.querySelector("#max-num-of-products") as HTML
 let chosen_op : string = "op1";
 let chosen_country: string = "ca";
 
+let product_list = document.querySelector(".product-list") as HTMLElement;
+
+let products_json: Product[] = [];
+
+function set_up_product_list(products : Product[]) : void {
+    product_list.textContent = "";
+
+    let minpriceval = min_price.value.trim();
+    if(minpriceval === ""){
+        minpriceval = "-999999";
+    }
+    let minpricefloat = parseFloat(minpriceval);
+
+    let maxpriceval = max_price.value.trim();
+    if(maxpriceval === ""){
+        maxpriceval = "999999";
+    }
+    let maxpricefloat = parseFloat(maxpriceval);
+
+    let minstarsval = min_num_of_stars.value.trim(); 
+    if(minstarsval === ""){
+        minstarsval = "-999999";
+    }
+    let minstarsfloat = parseFloat(minstarsval);
+
+    let maxstarsval = max_num_of_stars.value.trim(); 
+    if(maxstarsval === ""){
+        maxstarsval = "999999";
+    }
+    let maxstarsfloat = parseFloat(maxstarsval);
+
+    let minratingsval = min_num_of_ratings.value.trim(); 
+    if(minratingsval === ""){
+        minratingsval = "-999999";
+    }
+    let minratingsint = parseInt(minratingsval);
+
+    let maxratingsval = max_num_of_ratings.value.trim(); 
+    if(maxratingsval === ""){
+        maxratingsval = "999999";
+    }
+    let maxratingsint = parseInt(maxratingsval);
+
+    let maxproductsval = max_num_of_products.value.trim(); 
+    if(maxproductsval === ""){
+        maxproductsval = products.length.toString();
+    }
+    let maxproductsint = parseInt(maxproductsval);
+
+    for (let i = 0; i < maxproductsint; i++)
+    {
+        let product = products[i];
+
+        if((minpricefloat <= product.price && product.price <= maxpricefloat) && 
+           (minstarsfloat <= product.stars && product.stars <= maxstarsfloat) && 
+           (minratingsint <= product.ratings && product.ratings <= maxratingsint)){
+                product_list.insertAdjacentHTML("beforeend", `<li class="product">
+                    <img src="${product.image}">
+                    <a href="${product.url}" target="_blank">
+                        ${product.desc}
+                    </a>
+                    <span class="price">$${product.price}</span>
+                    <span class="num-of-stars">${product.stars} stars</span>
+                    <span class="num-of-ratings">${product.ratings} ratings</span>
+                </li>`);
+        }
+   }
+}
+
+let option_buttons = document.querySelectorAll(".option-btn");
+for(let i = 0; i < option_buttons.length; i++)
+{
+    let option_button = option_buttons[i] as HTMLElement; 
+    option_button.addEventListener("click", function(e) {
+        e.preventDefault();
+
+        if(e.target instanceof Element){
+            let options = document.querySelectorAll(".option-btn");
+            for(let i = 0; i < options.length; i++)
+            {
+                (options[i]).classList.remove("chosen-option");
+            }
+            e.target.classList.add("chosen-option");
+            chosen_op = e.target.id;
+
+            set_up_product_list(products_json);
+        }
+    });
+}
+
+document.querySelector("#form-submit").addEventListener("click", function(e) {
+    e.preventDefault();
+
+    set_up_product_list(products_json);
+});
+
 let send_message_to_server_list = document.querySelectorAll(".send-message-to-server");   
 for(let i = 0; i < send_message_to_server_list.length; i++)
 {
@@ -20,16 +129,6 @@ for(let i = 0; i < send_message_to_server_list.length; i++)
         e.preventDefault();
 
         if(e.target instanceof Element){
-            if(e.target.classList.contains("option-btn")){
-                let options = document.querySelectorAll(".option-btn");
-                for(let i = 0; i < options.length; i++)
-                {
-                    (options[i]).classList.remove("chosen-option");
-                }
-                e.target.classList.add("chosen-option");
-                chosen_op = e.target.id;
-            }
-
             if(e.target.classList.contains("img-country")){
                 let countries = document.querySelectorAll(".div-country");
                 for(let i = 0; i < countries.length; i++)
@@ -55,19 +154,20 @@ for(let i = 0; i < send_message_to_server_list.length; i++)
             if (this.readyState === 4 && this.status === 200){
                 console.log("Response Type " + this.responseType);
                 console.log("Response Text " + this.responseText);
+
+                let response = this.responseText;
+
+                let convertedResponse = JSON.parse(response);
+                products_json = convertedResponse;
+
+                set_up_product_list(products_json);
                 }
         };
         
         const apiURL = `${webURL}/search-product`;
         xhttp.open("POST", apiURL, true);
         xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xhttp.send("search_product="+search_product.value.trim()+"&min_price="+min_price.value.trim()
-                    +"&max_price="+max_price.value.trim()
-                    +"&min_num_of_stars="+min_num_of_stars.value.trim()+"&max_num_of_stars="+max_num_of_stars.value.trim()
-                    +"&min_num_of_ratings="+min_num_of_ratings.value.trim()
-                    +"&max_num_of_ratings="+max_num_of_ratings.value.trim()
-                    +"&max_num_of_products="+max_num_of_products.value.trim()
-                    +"&chosen_op="+chosen_op+"&chosen_country="+chosen_country);
+        xhttp.send("search_product="+search_product.value.trim()+"&chosen_country="+chosen_country);
     });
 }
 }
