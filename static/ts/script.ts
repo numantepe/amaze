27071e89d158b73d12606637eafb721edb7f715c
Numan Tepe <numantepe@numans-mbp.home>
2024-08-28 12:25:20 -0400

    some changes

--- static/ts/script.ts
+++ static/ts/script.ts
@@ -12,8 +12,7 @@ interface Product {
     max_ratings_max_stars_min_price: number
 }
 
-let webURL : string = "http://127.0.0.1:5000";
-//let webURL : string = "https://amazedeals.herokuapp.com";
+let webURL : string = window.location.origin;
 let search_product = document.querySelector("#search-product") as HTMLInputElement;
 let min_price = document.querySelector("#min-price") as HTMLInputElement;
 let max_price = document.querySelector("#max-price") as HTMLInputElement;
